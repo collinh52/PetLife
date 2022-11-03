f5da970db83025352732622b70bffcd7f79d1c30
Collin Hayes <collinh52@gmail.com>
2022-11-03 12:36:28 -0600

    fixed create tables and added user dummy data

---
+++ ProjectCode/src/init_data/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (username, password, profile_name, bio, profile_image_url, email, pet_type, birthday, joined_timestamp) VALUES
+('collin', '1234', 'Collin', 'This is my bio', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=640:*', 'coha2902@colorado.edu','Dog','2019-01-03','2022-11-03 13:23:44' ),
+('jonhson', '7890', 'John Doe', 'Info in my bio', '', 'johndoe@gmail.com','Cat','2015-11-03','2022-11-03 10:18:50' ),
+('fishlover5', '5678', 'Nemo', 'I live in Sydney', '', 'nemo@gmail.com','Fish','2019-10-08','2022-11-03 12:02:02' ),
+('mary', 'pass', 'Secretariat', 'Triple Crown Winner', '', 'faster_than_you@gmail.com','Horse','2010-07-08','2022-11-03 02:02:02' ),
+('tweet', '1111', 'Tweety', 'About me', '', 'tweet@gmail.com','Bird','2011-12-08','2022-11-03 02:02:02' );
+
