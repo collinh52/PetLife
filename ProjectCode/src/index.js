941da508908d80a432da9f10907496b887a9ca4f
eherniak <elhe5360@colorado.edu>
2022-11-12 10:40:46 -0700

    adding upload api

--- ProjectCode/src/index.js
+++ ProjectCode/src/index.js
@@ -1,28 +1,33 @@
 // importing the dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
-
-//importing cloudinary libraries
-var cloudinary = require('cloudinary');
-
+const path = require("path");
+const multer  = require('multer');
+const fs = require('fs');
+const cloudinary = require("cloudinary").v2;
 
 // defining the Express app
 const app = express();
 
+//setting up multer for file uploading
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + path.extname(file.originalname));
+    },
+})
+const upload = multer({ storage: storage })
+
+
 // using bodyParser to parse JSON in the request body into JS objects
 app.use(bodyParser.json());
 
-const message = 'Hey there!';
-
-// defining a default endpoint
-app.get('/', (req, res) => {
-  res.send(message);
-});
+app.set('view engine', 'ejs');
 
 const pgp = require('pg-promise')();
 require('dotenv').config();
-
-// TODO: uncomment next parts for when database is finished
 
 const dbConfig = {
     host: 'db',
@@ -34,14 +39,41 @@ const dbConfig = {
 
 const db = pgp(dbConfig);
 
-//////////////////////////////CLOUDINARY SETUP////////////////////////////
+app.get('/', (req, res) => {
+  res.render('pages/home');
+});
 
+//connecting to cloudinary db
 cloudinary.config({ 
   cloud_name: 'diaoicqwt', 
   api_key: '977326122325792', 
   api_secret: 'WnmdmGp6TPC4lNye657MmlmzorI' 
 });
 
+//upload api
+//image is uploaded, added to uploads folder, added to cloudinary, and deleted from uploads folder
+  app.post('/upload', upload.single('image'), async (req, res) => {
+
+        await cloudinary.uploader.upload(req.file.path)
+                  .then((result) => {
+                    res.status(200).send({
+                      message: "Image Successfully Uploaded",
+                      result,
+                    });
+                  }).catch((error) => {
+                    res.status(500).send({
+                      message: "Image Upload Failed",
+                      error,
+                    });
+                  });
+
+        await fs.unlink(req.file.path, (err) => {
+          if (err) {
+            console.error(err)
+            return
+          };
+        });
+  });
 
 
 app.listen(3000, () => {
